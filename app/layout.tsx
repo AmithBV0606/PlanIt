8022e562340657a791af2fa04c066b3230860e38
AmithBV0606 <amithrao0606@gmail.com>
2024-12-30 17:24:09 +0530

    Wrapped main component with ThemeProvider component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PlanIt",
@@ -24,10 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${inter.className}`}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {/* Header */}
+          <Header />
+          {/* Main */}
+          <main className="min-h-screen">{children}</main>
+          {/* Footer */}
+          <footer className="bg-gray-900 py-12"> 
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>Made with ❤️ by Amith B V</p>
+            </div>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
