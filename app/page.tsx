e4d910a11b3224090c85b19fdb25b6269e48020d
AmithBV0606 <amithrao0606@gmail.com>
2025-01-01 13:33:31 +0530

    Key features section completed.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
 import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { BarChart, Calendar, ChevronRight, Layout } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+
+const features = [
+  {
+    title: "Intuitive Kanban Boards",
+    description:
+      "Visualize your workflow and optimize team productivity with our easy-to-use Kanban boards.",
+    icon: Layout,
+  },
+  {
+    title: "Powerful Sprint Planning",
+    description:
+      "Plan and manage sprints effectively, ensuring your team stays focused on delivering value.",
+    icon: Calendar,
+  },
+  {
+    title: "Comprehensive Reporting",
+    description:
+      "Gain insights into your team's performance with detailed, customizable reports and analytics.",
+    icon: BarChart,
+  },
+];
 
 export default function Home() {
   return (
@@ -22,11 +44,13 @@ export default function Home() {
           </span>
         </h1>
 
-        <p className="text-xl text-gray-400 mb-10 max-w-3xl mx-auto font-bold">Empower your team with our intuitive project management solution.</p>
+        <p className="text-xl text-gray-400 mb-10 max-w-3xl mx-auto font-bold">
+          Empower your team with our intuitive project management solution.
+        </p>
 
         <Link href={"/onboarding"}>
           <Button size={"lg"} className="mr-4">
-            Get Started <ChevronRight size={18}  />
+            Get Started <ChevronRight size={18} />
           </Button>
         </Link>
 
@@ -35,6 +59,27 @@ export default function Home() {
             Learn more
           </Button>
         </Link>
+      </section>
+
+      {/* Key features */}
+      <section className="bg-gray-900 py-20 px-5" id="features">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Key Features</h3>
+
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => {
+              return (
+                <Card key={index}>
+                  <CardContent className="pt-6">
+                    <feature.icon className="h-12 w-12 mb-4 text-blue-500"/>
+                    <h4 className="text-xl font-semibold mb-2">{feature.title}</h4>
+                    <p className="text-gray-400">{feature.description}</p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
       </section>
     </div>
   );
