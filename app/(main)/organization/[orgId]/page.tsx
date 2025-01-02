08674ccacc3f3408d271882577c8ffafc1ed8ee2
AmithBV0606 <amithrao0606@gmail.com>
2025-01-02 14:07:02 +0530

    Completed 50% of Organization page.

--- app/(main)/organization/[orgId]/page.tsx
+++ app/(main)/organization/[orgId]/page.tsx
@@ -1,7 +1,8 @@
 import { getOrganization } from "@/actions/organization";
+import OrgSwitcher from "@/components/org-switcher";
 
 const Organization = async ({ params }: any) => {
-  const { orgId } = params;
+  const { orgId } = await params; // I have awaited only after getting the error messagge.
   const organization = await getOrganization({ slug: orgId });
 
   if (!organization) {
@@ -9,10 +10,17 @@ const Organization = async ({ params }: any) => {
   }
 
   return (
-    <div>
-      <div>
-        <h1>{organization.name}&rsquo;s Projects</h1>
+    <div className="container mx-auto">
+      <div className="mb-4 flex flex-col sm:flex-row justify-between items-start">
+        <h1 className="text-5xl font-bold gradient-title-2">{organization.name}&rsquo;s Projects</h1>
+
+        {/* Org switcher */}
+        <OrgSwitcher />
       </div>
+
+      <div className="mb-4">Show org projects</div>
+
+      <div className="mt-8">Show user assigned and reported issues here</div>
     </div>
   );
 };
