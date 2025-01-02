2cbd72629b25067cb95fdefc2110fe0ef8f72729
AmithBV0606 <amithrao0606@gmail.com>
2025-01-02 09:54:42 +0530

    Sanitized "/organization/..." path.

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,9 @@
-import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+  auth,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // This variable will have all the routes, which should be kept in private for unauthorized users.
 const isProtected = createRouteMatcher([
@@ -12,13 +17,22 @@ export default clerkMiddleware((auth, req) => {
   if (!auth().userId && isProtected(req)) {
     return auth().redirectToSignIn();
   }
+
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== "/onboarding" &&
+    req.nextUrl.pathname !== "/"
+  ) {
+    return NextResponse.redirect(new URL("/onboarding", req.url));
+  }
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
